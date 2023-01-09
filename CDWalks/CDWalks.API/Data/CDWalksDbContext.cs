3aefa57ac44f02284635a4491b63cbc6aaffcbbc
devika <devikamurali08@gmail.com>
2023-01-09 21:40:54 +0530

    END OF SECTION2

---
+++ CDWalks/CDWalks.API/Data/CDWalksDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CDWalks.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CDWalks.API.Data
+{
+    public class CDWalksDbContext:DbContext
+    {
+        public CDWalksDbContext(DbContextOptions<CDWalksDbContext>options):base(options)
+        {
+
+        }
+        public DbSet<Region>Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+    }
+}
